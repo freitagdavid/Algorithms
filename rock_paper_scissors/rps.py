71f39b508527a768db9784217297a0cbbe0ee711
David Freitag <freitagdavid@outlook.com>
2019-04-01 18:37:04 -0400

    rock_paper_scissors/rps: Complete

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -1,14 +1,22 @@
 #!/usr/bin/python
 
 import sys
+import itertools
+
+plays = ['rock', 'paper', 'scissors']
+
 
 def rock_paper_scissors(n):
-  pass 
+    working_output = itertools.product(plays, repeat=n)
+    output = []
+    for i in working_output:
+        output.append(list(i))
+    return output
 
 
 if __name__ == "__main__":
-  if len(sys.argv) > 1:
-    num_plays = int(sys.argv[1])
-    print(rock_paper_scissors(num_plays))
-  else:
-    print('Usage: rps.py [num_plays]')
+    if len(sys.argv) > 1:
+        num_plays = int(sys.argv[1])
+        print(rock_paper_scissors(num_plays))
+    else:
+        print('Usage: rps.py [num_plays]')
